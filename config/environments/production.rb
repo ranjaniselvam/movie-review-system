8c94d4ca1180e35a35775d882fb9232d8ab2accd
Sivaranjani Selvam <sivaranjani@mallow-tech.com>
2023-04-28 12:53:52 +0530

    final commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -130,8 +130,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'sivaranjani.sn12@gmail.com',
-    password:             'puttu2023',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
